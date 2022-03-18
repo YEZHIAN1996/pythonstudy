9d25648ca0a3eea5b3f778860f5c21f4e9619ed5
YEZHIAN1996 <87108112+YEZHIAN1996@users.noreply.github.com>
2022-03-18 19:52:11 +0800

    数字猜猜猜

--- game_number/mooc_test.py
+++ game_number/mooc_test.py
@@ -1,46 +1,84 @@
 # coding:utf-8
 import sys
 import datetime
-def guide_page(guide_word):
-    return '***************{}**********************'.format(guide_word)
+import random
 
-def all_num(n):
-    if not n.isdigit():
-        return '您输入的为非数字字符，请重新启动！'
-    else:
-        return n
+class Game_num(object):
+    # def __init__(self):
+    #     pass
 
-def num_legal(ls):
-    if int(ls[0]) == int(ls[1]):
+    # 游戏进入提示
+    def guide_page(self, guide_word):
+        return '***************{}**********************'.format(guide_word)
+
+    # 判断是否为整数
+    def all_num(self, n):
+        return n.isdigit()
+
+    # 数字合法性判断函数
+    def num_legal(self, ls):
+        self.ls = ls
+        if int(self.ls[0]) == int(self.ls[-1]):
+            print('您输入的区间数字相同！！请重新启动程序。')
+        elif int(self.ls[0]) > int(self.ls[-1]):
+            print('您输入的数字区间大小有误！！请重新启动程序。')
+        else:
+            return 1
         sys.exit()
-        return '您输入的区间数字相同！！请重新启动程序。'
-    elif int(ls[0]) > int(ls[1]):
-        sys.exit()
-        return '您输入的数字区间大小有误！！请重新启动程序。'
-    else:
-        return 1
 
-def set_final_num(num1, num2):
-    list_ = [num1, num2]
-    new_list = list(filter(all_num, list_))
-    # num_legal(new_list)
-    return new_list
+    # 产生指定区间的随机数
+    def set_final_num(self, num1, num2):
+        list_ = [num1, num2]
+        new_list = list(filter(self.all_num, list_))
 
+        if len(new_list) == 2:
+            returnValue = self.num_legal(new_list)
+            if returnValue == 1:
+                print('所产生的随机数字区间为: %s' % list_)
+                return random.randint(int(num1), int(num2))
+        else:
+            print('您所输入的为非数字字符, 请重新启动！')
+            sys.exit()
 
+    def check_num_legal(self, num):
+        if int(self.ls[0]) < int(num) < int(self.ls[1]):
+            return 1
+        else:
+            print('对不起您输入的数字未在指定区间！！！')
 
-def check_num_legal(num):
-    pass
-def write_record(times, value):
-    # now = datetime.datetime.now()
-    for i in times:
+    def write_record(self, times, value):
+        string = '第{}次您猜测的数字是{}'.format(times, value)
         now = datetime.datetime.now()
-        string = f'{now}:第{i}次您猜测的数字为：{value[i]}'
-        with open('record.txt') as f:
-            f.write(string, 'wb')
+        with open('record.txt', 'a', encoding='utf-8') as f:
+            f.write(f'\n{now} : {string}')
 
+    def main(self, rand1):
+        temp = 0
+        while True:
+            input_num = input('请继续输入您猜测的数字:')
+            if self.all_num(input_num):
+                input_num = int(input_num)
+            else:
+                print('您所输入的为非数字字符, 请重新启动！')
+                sys.exit()
+            if self.check_num_legal(input_num):
+                continue
+            temp += 1
+            self.write_record(temp, input_num)
+            print('*' * 10)
 
-
+            if input_num < rand1:
+                print('Lower than the answer')
+            elif input_num > rand1:
+                print('Higher than the answer')
+            else:
+                print('恭喜您！只用了%s次就赢得了游戏' % temp)
+                break
 
 if __name__ == '__main__':
-    print(guide_page('欢迎进入数字猜猜猜小游戏'))
-    print(set_final_num(1, 2))
+    guess_num = Game_num()
+    print(guess_num.guide_page('欢迎进入数字猜猜猜小游戏'))
+    num1 = input('数字区间起始值:')
+    num2 = input('数字区间终止值:')
+    rand = guess_num.set_final_num(num1, num2)
+    guess_num.main(rand)
